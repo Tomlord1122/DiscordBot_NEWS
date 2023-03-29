696ad6cc0f583ecd00229eac3e2c1c447a20068d
TomLiu <aa123593465@gmail.com>
2023-03-29 17:11:39 +0800

    hey

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='[', intents=discord.Intents.all(), case_insensitive=True, self_bot=True)  #bot 代表機器人 prefix = 命令字首，要有這個prefix才能命令機器人
+bot = commands.Bot(command_prefix='.', intents=discord.Intents.all(), case_insensitive=True, self_bot=True)  #bot 代表機器人 prefix = 命令字首，要有這個prefix才能命令機器人
 
 
 @bot.event # @ = 裝飾器  
@@ -16,6 +16,12 @@ async def on_member_join(member):
 async def on_member_remove(member):
     channel = bot.get_channel(1090560796171448330)
     await channel.send(F'{member} leave!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(F'{round(bot.latency*1000)}(ms)') #回傳bot 的延遲 (ms)
+
+
 
 
 
