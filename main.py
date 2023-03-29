dde98e2bf9cdec7016b9db588be65a21535e4adb
TomLiu <aa123593465@gmail.com>
2023-03-29 17:34:42 +0800

    引入Json概念

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import discord
 from discord.ext import commands
+import json #寫好設定檔 防止token被盜
+
+
+with open('setting.json', 'r', encoding='utf8') as jfile:  #讀取json設定檔
+    jdata = json.load(jfile)
+
 
 bot = commands.Bot(command_prefix='.', intents=discord.Intents.all(), case_insensitive=True, self_bot=True)  #bot 代表機器人 prefix = 命令字首，要有這個prefix才能命令機器人
 
@@ -10,11 +16,11 @@ async def on_ready():  #async 協成應用函式
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(1090560780832878622)
+    channel = bot.get_channel(int(jdata["welcome_channel"]))
     await channel.send(F'{member} join!')
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(1090560796171448330)
+    channel = bot.get_channel(int(jdata["1090560796171448330"]))
     await channel.send(F'{member} leave!')
 
 @bot.command()
@@ -27,7 +33,7 @@ async def ping(ctx):
 
 
 
-bot.run("MTA4OTE0MDQ5MTk1NTczNjY2Ng.Gg9blX.iZAQFHd5Sd9kD_kpTTU3A0iQyIIqqI-c_dEbNU")  #放token並啟動
+bot.run(jdata["TOKEN"])  #放token並啟動
 
 
 
